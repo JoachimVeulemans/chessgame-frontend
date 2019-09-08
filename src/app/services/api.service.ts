8c1fb6706e538bbb8187e53e7b58e99fdc4f3937
Joachim Veulemans <joachim@cloudclass.be>
2019-09-09 01:42:56 +0200

    Convert to string for now

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,4 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Chessboard } from '../data/classes/chessboard';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -15,9 +14,9 @@ export class ApiService {
 
     constructor(private http: HttpClient) { }
 
-    getChessboard(): Observable<Chessboard> {
+    getChessboard(): Observable<string[]> {
         const url = `${this._backend_url}/chess`;
 
-        return this.http.get<Chessboard>(url, this.optionsWithoutCredentials);
+        return this.http.get<string[]>(url, this.optionsWithoutCredentials);
     }
 }
