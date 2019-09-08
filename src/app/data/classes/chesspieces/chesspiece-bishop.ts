f9d6ec9ddecf9f291939e6beaae89a4a8224ad9e
Joachim Veulemans <joachim@cloudclass.be>
2019-09-09 01:42:32 +0200

    Added color to chesspiece classes

--- src/app/data/classes/chesspieces/chesspiece-bishop.ts
+++ src/app/data/classes/chesspieces/chesspiece-bishop.ts
@@ -1,7 +1,14 @@
 import { Chesspiece } from './chesspiece-abstract';
 
 export class ChesspieceBishop extends Chesspiece {
-    _icon = 'assets/images/chesspieces/bishop_white.png';
+    _icon = 'assets/images/chesspieces/bishop';
     _name = 'Bishop';
     _description = '';
+    _color = '';
+
+    constructor(color: string) {
+        super();
+        this._color = color;
+        this._icon = this._icon + '_' + this._color + '.png';
+    }
 }
--- src/app/data/classes/chesspieces/chesspiece-knight.ts
+++ src/app/data/classes/chesspieces/chesspiece-knight.ts
@@ -1,7 +1,14 @@
 import { Chesspiece } from './chesspiece-abstract';
 
 export class ChesspieceKnight extends Chesspiece {
-    _icon = 'assets/images/chesspieces/knight_white.png';
+    _icon = 'assets/images/chesspieces/knight';
     _name = 'Knight';
     _description = '';
+    _color = '';
+
+    constructor(color: string) {
+        super();
+        this._color = color;
+        this._icon = this._icon + '_' + this._color + '.png';
+    }
 }
