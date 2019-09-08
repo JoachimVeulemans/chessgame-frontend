89c55d283027c6a390eef9aec0e20c9649db2e53
Joachim Veulemans <joachim@cloudclass.be>
2019-09-09 01:42:32 +0200

    Added color to chesspiece classes

--- src/app/data/classes/chesspieces/chesspiece-abstract.ts
+++ src/app/data/classes/chesspieces/chesspiece-abstract.ts
@@ -1,5 +1,9 @@
 export abstract class Chesspiece {
+
+    constructor() { }
+
     abstract _name: string;
     abstract _icon: string;
     abstract _description: string;
+    abstract _color: string;
 }
--- src/app/data/classes/chesspieces/chesspiece-pawn.ts
+++ src/app/data/classes/chesspieces/chesspiece-pawn.ts
@@ -1,7 +1,14 @@
 import { Chesspiece } from './chesspiece-abstract';
 
 export class ChesspiecePawn extends Chesspiece {
-    _icon = 'assets/images/chesspieces/pawn_white.png';
+    _icon = 'assets/images/chesspieces/pawn';
     _name = 'Pawn';
     _description = 'oh';
+    _color = '';
+
+    constructor(color: string) {
+        super();
+        this._color = color;
+        this._icon = this._icon + '_' + this._color + '.png';
+    }
 }
