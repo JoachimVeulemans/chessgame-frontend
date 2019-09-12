55bcdda219e20be7190a6cce628a1556722ba632
Joachim Veulemans <joachim@cloudclass.be>
2019-09-12 18:27:27 +0200

    Updated ApiService

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,9 @@
+import * as shajs from 'sha.js';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Game } from '../data/classes/Game';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { User } from '../data/classes/User';
 import { environment } from 'src/environments/environment';
 
 /** This service can be used to perform API calls */
@@ -22,6 +25,136 @@ export class ApiService {
 
     /** Constructor */
     constructor(private http: HttpClient) { }
+
+    /**
+     * GENERAL
+     */
+
+    login(username: string, password: string, salt: string): Observable<Boolean> {
+        const url = `${this._backend_url}/login`;
+
+        password = shajs('sha256').update(password + salt).digest('hex');
+        const body = {
+            username: username,
+            password: password
+        };
+
+        return this.http.post<Boolean>(url, body, this._optionsWithCredentials);
+    }
+
+    getNewSalt(): Observable<string> {
+        const url = `${this._backend_url}/salt`;
+
+        return this.http.get<string>(url, this._optionsWithCredentials);
+    }
+
+    getUserSalt(username: string): Observable<string> {
+        const url = `${this._backend_url}/salt/${username}`;
+
+        return this.http.get<string>(url, this._optionsWithCredentials);
+    }
+
+    loggedin(): Observable<Boolean> {
+        const url = `${this._backend_url}/loggedin`;
+
+        return this.http.get<Boolean>(url, this._optionsWithCredentials);
+    }
+
+    logout(): Observable<Boolean> {
+        const url = `${this._backend_url}/logout`;
+
+        return this.http.post<Boolean>(url, {}, this._optionsWithCredentials);
+    }
+
+    checkIfUsernameAvailable(username: string): Observable<Boolean> {
+        const url = `${this._backend_url}/checkusername/${username}`;
+
+        return this.http.get<Boolean>(url, this._optionsWithCredentials);
+    }
+
+    /**
+     * USER
+     */
+
+    getAllUsers(): Observable<User[]> {
+        const url = `${this._backend_url}/users`;
+
+        return this.http.get<User[]>(url, this._optionsWithoutCredentials);
+    }
+
+    getUser(id: string): Observable<User> {
+        const url = `${this._backend_url}/users/${id}`;
+
+        return this.http.get<User>(url, this._optionsWithoutCredentials);
+    }
+
+    getUserMe(): Observable<User> {
+        const url = `${this._backend_url}/users/me`;
+
+        return this.http.get<User>(url, this._optionsWithoutCredentials);
+    }
+
+    createUser(user: User): Observable<User> {
+        user.password = shajs('sha256').update(user.password + user.salt).digest('hex');
+        const url = `${this._backend_url}/users`;
+
+        return this.http.post<User>(url, user, this._optionsWithCredentials);
+    }
+
+    editUser(user: User) {
+        const url = `${this._backend_url}/users/${user.id}`;
+
+        return this.http.put(url, user, this._optionsWithCredentials);
+    }
+
+    editUserPassword(user: User) {
+        user.password = shajs('sha256').update(user.password + user.salt).digest('hex');
+        const url = `${this._backend_url}/users/${user.id}`;
+        const body = {
+            id: user.id,
+            password: user.password
+        }
+
+        return this.http.put(url, body, this._optionsWithCredentials);
+    }
+
+    deleteUser(user: User) {
+        const url = `${this._backend_url}/users/${user.id}`;
+
+        return this.http.delete(url, this._optionsWithCredentials);
+    }
+
+    /**
+     * Games
+     */
+
+    getAllGames(): Observable<Game[]> {
+        const url = `${this._backend_url}/games`;
+
+        return this.http.get<Game[]>(url, this._optionsWithoutCredentials);
+    }
+
+    getMyGames(): Observable<Game[]> {
+        const url = `${this._backend_url}/games/me`;
+
+        return this.http.get<Game[]>(url, this._optionsWithoutCredentials);
+    }
+
+    getGame(id: string): Observable<Game> {
+        const url = `${this._backend_url}/games/${id}`;
+
+        return this.http.get<Game>(url, this._optionsWithoutCredentials);
+    }
+
+    deleteGame(game: Game) {
+        const url = `${this._backend_url}/messages/${game.id}`;
+
+        return this.http.delete(url, this._optionsWithCredentials);
+    }
+
+    /**
+     * CHESSBOARD
+     */
 
     /** Returns the chessboard's chesspieces in correct order */
     getChessboard(): Observable<string[]> {
