c94298c4f4c74679194730d20f8cb7fd163b68a4
Joachim Veulemans <joachim@cloudclass.be>
2019-09-12 11:51:15 +0200

    Add profile page

--- src/app/pages/profile/components/profile/profile.component.ts
+++ src/app/pages/profile/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 /** Defines the selector and location of the HTML & SCSS */
 @Component({
@@ -8,8 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
     /** Constructor */
-    constructor() { }
+    constructor(private apiService: ApiService) { }
 
     /** Defines what should be done next when the component is created */
     ngOnInit() { }
+
+    saveProfile(firstname: string, lastname: string, email: string, bio: string, language: string) {
+        console.log(firstname + lastname + email + bio + language);
+    }
+
+    savePassword(password: string, password2: string) {
+        console.log(password + password2);
+    }
+
+    deleteAccount() {
+        console.log('delete account requested');
+    }
+
+    private setProfileButtonsStateNormal() {
+
+    }
+
+    private setProfileButtonsStateSaving() {
+
+    }
+
+    private setProfileButtonsStateSuccess() {
+
+    }
+
+    private setProfileButtonsStateError() {
+
+    }
+
+    private setPasswordButtonsStateNormal() {
+
+    }
+
+    private setPasswordButtonsStateSaving() {
+
+    }
+
+    private setPasswordButtonsStateSuccess() {
+
+    }
+
+    private setPasswordButtonsStateError() {
+
+    }
+
+    private setDeleteAccountButtonsStateNormal() {
+
+    }
+
+    private setDeleteAccountButtonsStateDeleting() {
+
+    }
+
+    private setDeleteAccountButtonsStateSuccess() {
+
+    }
+
+    private setDeleteAccountButtonsStateError() {
+
+    }
+
+
 }
