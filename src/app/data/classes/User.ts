1a27311d691f855f4aa7ac804794de2e56123515
Joachim Veulemans <joachim@cloudclass.be>
2019-09-12 18:27:47 +0200

    Added User class

---
+++ src/app/data/classes/User.ts
@@ -0,0 +1,34 @@
+export class User {
+    id: string;
+    username: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    bio: string;
+    password: string;
+    salt: string;
+    language: string;
+
+    /** constructor */
+    constructor(id: string = '', username: string = '', firstname: string = '', lastname: string = '', email: string = '', bio: string = '', password: string = '', salt: string = '', language: string = '') {
+        this.id = id;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.bio = bio;
+        this.password = password;
+        this.salt = salt;
+        this.language = language;
+    }
+
+    editProfile(user: User, firstname: string, lastname: string, email: string, bio: string, language: string): User {
+        user.firstname = firstname;
+        user.lastname = lastname;
+        user.email = email;
+        user.bio = bio;
+        user.language = language;
+
+        return user;
+    }
+}
