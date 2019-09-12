36da797e4fa8d6a9806f3fdc84babb48da81c483
Joachim Veulemans <joachim@cloudclass.be>
2019-09-12 09:40:36 +0200

    Added loading icon

--- src/app/standard-components/loading-icon/loading-icon.component.ts
+++ src/app/standard-components/loading-icon/loading-icon.component.ts
@@ -11,5 +11,25 @@ export class LoadingIconComponent implements OnInit {
     constructor() { }
 
     /** Defines what should be done next when the component is created */
-    ngOnInit() { }
+    ngOnInit() {
+        this.restartAnimation();
+    }
+
+    /** Restart the animation every 4s */
+    private restartAnimation() {
+        setInterval(() => { this.restartCSS() }, 4000);
+    }
+
+    /** Removes the old board & calls the reintroduce function after 0.1s */
+    private restartCSS() {
+        $('.pawnpiece').removeClass('pawnimate');
+        $('#wrapper').removeClass('restart-css');
+        setTimeout(() => { this.reintroduce() }, 100);
+    }
+
+    /** Reintroduces the board */
+    private reintroduce() {
+        $('.pawnpiece').addClass('pawnimate');
+        $('#wrapper').addClass('restart-css');
+    }
 }
