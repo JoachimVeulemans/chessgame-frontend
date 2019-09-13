071fabdd46edc1fdcc2a0baabb44d482d659fe2c
Joachim Veulemans <joachim@cloudclass.be>
2019-09-13 16:22:59 +0200

    Added login & register component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { StandardComponentsModule } from 'src/app/standard-components/standard-components.module';
 import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     declarations: [
-        HomeComponent
+        HomeComponent,
+        LoginComponent
     ],
     imports: [
         CommonModule,
@@ -17,6 +19,7 @@ import { TranslateModule } from '@ngx-translate/core';
     ],
     exports: [
         HomeComponent,
+        LoginComponent,
         TranslateModule
     ]
 })
